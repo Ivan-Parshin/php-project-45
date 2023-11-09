0f9da7d1dea8b70b8ea8c1355dfc9022c12758e7
IvanP <p2.2011@yandex.ru>
2023-11-09 13:54:21 +0300

    55

--- src/Even.php
+++ src/Even.php
@@ -3,34 +3,26 @@
 use function cli\line;
 use function cli\prompt;
 
-
-
 function even()
 {
     line('Welcome to the Even Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-
     line('Answer "yes" if the number is even, otherwise answer "no"');
 
-    for($i = 0; $i < 3; ){
+    for ($i = 0; $i < 3; $i++) {
         $number = rand(1, 100);
-    $answer = prompt("Question: {$number}");
-    line("You answer: {$answer}");
-    $correctAnswer = $number % 2 === 0 ? 'yes' : 'no';
-    // return [$answer, $correctAnswer];
-    if($correctAnswer === $answer) {
-        line( "Correct!");
-        $i++;
-    } else {
-        return line("'yes' is wrong answer ;(. Correct answer was 'no'.
-        Lets try again, {$name}");
+        $answer = prompt("Question: {$number}");
+        line("You answered: {$answer}");
+        $correctAnswer = $number % 2 === 0 ? 'yes' : 'no';
 
-    };
-    
+        if ($correctAnswer === $answer) {
+            line("Correct!");
+        } else {
+            return line("'{$answer}' is the wrong answer. The correct answer was '{$correctAnswer}'. Let's try again, {$name}!");
+            // return even(); // Recursive call to restart the game
+        }
+    }
 
+    line("Congratulations, {$name}!");
 }
-line("Congratulations, {$name}!");
-
-}
-
